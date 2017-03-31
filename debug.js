91db2353371846f8e46608865818ca8008d36aed
gabriele99g <gabriele.giuli.dev@gmail.com>
2017-03-30 20:52:57 -0700

    Comment added

--- debug.js
+++ debug.js
@@ -1,3 +1,10 @@
+// Snake.io - debug.js
+//
+// Simple online game that allows multiple players to play snake together,
+// merging the classic arcade game with the concept of slither.io
+//
+// Gabriele Giuli, All rights reserved
+
 var config = require('./config');
 
 module.exports = function(text) {
